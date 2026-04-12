6e5f358a6fbc83ad047ed3c0fa25f291e6d84c0e
HugoRodrigues01 <hugo999junha@gmail.com>
2026-04-12 00:32:27 -0300

    fix: refectory user schema

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,12 +1,12 @@
-from datetime import datetime
+from datetime import datetime, UTC
 
 from pydantic import BaseModel
 
 
 class UserSchema(BaseModel):
     username: str
-    number: int
+    phone_number: int
     user_email: str
     password: str
-    created_at: datetime
-    updated_at: datetime
+    created_at: datetime = datetime.now(UTC)
+    updated_at: datetime = datetime.now(UTC)
