309046cc86e753c89dd3cc6c0481bf95da702182
HugoRodrigues01 <hugo999junha@gmail.com>
2026-04-12 14:28:55 -0300

    feat(user): add update_user function
    Update an user if already exists.

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 
 from src.database import get_session
 from src.models.users import User
-from src.schemas.users import UserSchema
+from src.schemas.users import UserSchema, UserUpdateSchema
 from src.views.users import UserListView, UserView
 
 app = FastAPI()
@@ -73,9 +73,34 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
     return db_user
 
 
-@app.put("/usuarios/{id}", status_code=200)
-def update_user(id: int):
-    pass
+@app.put("/usuarios/{id}", status_code=HTTPStatus.CREATED, response_model=UserView)
+def update_user(
+    id: int, 
+    new_user: UserUpdateSchema, 
+    session: Session=Depends(get_session)
+):
+    user = session.scalar(
+        select(User).where(User.user_id == id)
+    )
+
+    if user:
+        update_user = user
+        update_user.username = new_user.username
+        update_user.user_email = new_user.user_email or user.user_email
+        update_user.password = new_user.password or user.password
+        update_user.phone_number = new_user.phone_number or user.phone_number
+
+        session.add(update_user)
+        session.commit()
+        session.refresh(update_user)
+
+    else:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND,
+            detail="User do not exists."
+        )
+
+    return update_user
 
 
 @app.delete("/usuarios/{id}")
