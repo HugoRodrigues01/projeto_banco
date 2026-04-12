b4bbe29a7e4054dd5e1267b338280b5aef7d5d62
HugoRodrigues01 <hugo999junha@gmail.com>
2026-04-12 00:27:34 -0300

    feat: add create_user function

--- src/app.py
+++ src/app.py
@@ -1,9 +1,14 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+
+from sqlalchemy import insert, select
+from sqlalchemy.orm import Session
 
 from src.schemas.users import UserSchema
 from src.views.users import UserView
+from src.models.users import User 
+from src.database import get_session
 
 app = FastAPI()
 
@@ -25,8 +30,38 @@ def get_user(id: int):
 
 
 @app.post("/usuarios", status_code=HTTPStatus.CREATED, response_model=UserView)
-def create_user(user: UserSchema):
-    pass
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.user_email == user.user_email)
+        )
+    )
+
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail="Username already exists."
+            )
+        elif db_user.username == user.username:
+            raise HTTPException(
+                status_code=HTTPStatus.CONFLICT,
+                detail="Use email already exists."
+            )
+    
+    db_user = User(
+        username=user.username,
+        user_email=user.user_email,
+        phone_number=user.phone_number,
+        password=user.password
+    )
+
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.put("/usuarios/{id}", status_code=200)
