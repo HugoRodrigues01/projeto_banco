305cceb10ae5235a4754c818898daf5063dcab7f
HugoRodrigues01 <hugo999junha@gmail.com>
2026-04-12 14:28:02 -0300

    feat(user): add new schema for update user function

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -10,3 +10,10 @@ class UserSchema(BaseModel):
     password: str
     created_at: datetime = datetime.now(UTC)
     updated_at: datetime = datetime.now(UTC)
+
+
+class UserUpdateSchema(BaseModel):
+    username: str
+    phone_number: int
+    user_email: str
+    password: str
