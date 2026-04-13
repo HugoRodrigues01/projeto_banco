7afdab78df9be26bb195a62c4267077c7e930cd2
HugoRodrigues01 <hugo999junha@gmail.com>
2026-04-13 00:08:51 -0300

    feat(token): add access token for login

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
 from src.database import get_session
-from src.security import create_password_hash
 from src.models.users import User
 from src.schemas.users import UserSchema, UserUpdateSchema
+from src.schemas.token import TokenSchema
+from src.security import create_password_hash, verify_password, create_access_token
 from src.views.users import UserListView, UserView
 
 app = FastAPI()
@@ -16,6 +18,29 @@ app = FastAPI()
 @app.get("/")
 def hello():
     return {"Message": "Hello World"}
+
+@app.get("/token", response_model=TokenSchema)
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    session: Session=Depends(get_session)
+):
+    
+    user = session.scalar(select(User).where(User.user_email == form_data.username))
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Iconrrect email or password"
+        )
+    
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail="Iconrrect email or password"
+        )
+    
+    access_token = create_access_token({"sub": user.user_email})
+    return {"access_token": access_token, "token_type": "bearer"}
 
 
 # USUARIOS
